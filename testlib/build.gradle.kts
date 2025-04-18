950f4ce891b7cf0a892f1c62c59b410987dee595
IResetic <iresetic.skybit@gmail.com>
2025-04-18 17:18:10 +0200

    Update 5

--- testlib/build.gradle.kts
+++ testlib/build.gradle.kts
@@ -51,7 +51,7 @@ afterEvaluate {
                 url = uri("https://maven.pkg.github.com/IResetic/testgithubpackages")
                 credentials {
                     username = "IResetic"
-                    password = "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"
+                    password = "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"
                 }
             }
         }
