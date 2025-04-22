ee91bf370f72400172c0a94c060c89af3bb22212
IResetic <iresetic.skybit@gmail.com>
2025-04-22 09:56:19 +0200

    New repo modul

---
+++ kombo/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.example.kombo"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 28
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.example.kombo"
+                artifactId = "kombo"
+                version = "1.0.0"
+            }
+        }
+
+        repositories {
+            maven {
+                name = "GitHubPackagesTest"
+                url  = uri("https://maven.pkg.github.com/IResetic/testgithubpackages")
+                credentials {
+                    username = project.findProperty("gpr.user") as String
+                    password = project.findProperty("gpr.key")  as String
+                }
+            }
+        }
+    }
+}
---
+++ kombo/two/src/main/java/com/example/two/KomboTwo.kt
@@ -0,0 +1,7 @@
+package com.example.two
+
+class KomboTwo {
+    fun printKomboTwo() {
+        println("Hello from KomboTwo!")
+    }
+}
